9b88500f055387b818b0d510a69927108dc616dd
Jagadeeshbabu2000 <jagadeeshbabu486@gmail.com>
2025-08-14 21:13:55 +0530

    Initial Commit

---
+++ ShopApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShopApp
+//
+//  Created by K V Jagadeesh babu on 14/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
